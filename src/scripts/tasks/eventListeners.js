bb1a4ad21037c840bd6887954a69590d6669e4c4
George Williamson <keatonwilliamson@Georges-MacBook-Pro.local>
2019-09-23 20:21:41 -0500

    post is working, async isuues

--- src/scripts/tasks/eventListeners.js
+++ src/scripts/tasks/eventListeners.js
@@ -6,7 +6,27 @@ const taskObject = {
     },
     taskNav: () => {
         document.querySelector("#btnDailyTasks").addEventListener("click", event => {
-            console.log("you clllickkedd")
+            taskObject.taskRefresh();
+            // console.log("you clllickkedd")
+            // let mainContainer = document.querySelector("#mainContainer")
+            // mainContainer.innerHTML = ""
+            // mainContainer.innerHTML += taskObject.taskModalComponent();
+            // document.querySelector("#addContainer").innerHTML = ' Heyyyy daily Tasks <button type="button" id="addTask">Add Task</button> '
+            // document.querySelector("#addTask").addEventListener("click", event => {
+            //     document.querySelector("#tasksModal").style.display = "block";
+            //     document.querySelector("#btnCloseTasks").addEventListener("click", event => {
+            //         document.querySelector("#tasksModal").style.display = "none";
+            //     })
+            //     document.querySelector("#btnTasksSave").addEventListener("click", event => {
+            //         taskObject.postJSON();
+            //     })
+            // })
+            // console.log(sessionStorage.activeUser);
+            // API.searchGet("tasks", "userId", sessionStorage.activeUser)
+            //     .then(response => taskObject.taskLoop(response));
+        })
+    },
+    taskRefresh: () => {
             let mainContainer = document.querySelector("#mainContainer")
             mainContainer.innerHTML = ""
             mainContainer.innerHTML += taskObject.taskModalComponent();
@@ -16,12 +36,15 @@ const taskObject = {
                 document.querySelector("#btnCloseTasks").addEventListener("click", event => {
                     document.querySelector("#tasksModal").style.display = "none";
                 })
+                document.querySelector("#btnTasksSave").addEventListener("click", event => {
+                    taskObject.postJSON();
+                })
             })
             console.log(sessionStorage.activeUser);
             API.searchGet("tasks", "userId", sessionStorage.activeUser)
                 .then(response => taskObject.taskLoop(response));
-        })
     },
+
     taskModalComponent: () => {
         return `
             <section id="tasksContainer"></section>
@@ -35,8 +58,9 @@ const taskObject = {
                         <div class="flex-container">
                             <input class="flex-input" type="text" placeholder=" Title" id="taskTitle">
                             <input class="flex-input" type="text" placeholder="Write a task..." id="taskDetails">
+                            <input class="flex-input" type="date" id="taskDate">
                         </div>
-                        <button id="btnNewsSave">Submit</button>
+                        <button id="btnTasksSave">Submit</button>
                     </div>
                 </div>
             </div> 
@@ -47,8 +71,8 @@ const taskObject = {
     },
     taskComponent: (task) => {
         return `<div>
-            <h1>theeeee  ${task.taskName}</h1>
-            <p> eeeeeyyah${task.dueDate}</p>
+            <h1>title  ${task.taskName}</h1>
+            <p>date ${task.dueDate}</p>
         </div>`
     },
     taskLoop: (taskArray) => {
@@ -57,7 +81,26 @@ const taskObject = {
             console.log(taskObject.taskComponent(task));
         });
     },
+    jsonObject: (title, details, date) => {
+        return {
+            userId: sessionStorage.activeUser,
+            taskName: title,
+            taskDescription: details,
+            dueDate: date,
+            isComplete: "no"
+        }
+    },
+    postJSON: () => {
+        let title = document.querySelector("#taskTitle").value
+        let deets = document.querySelector("#taskDetails").value
+        let date = document.querySelector("#taskDate").value
+        API.saveAnything(taskObject.jsonObject(title, deets, date), "tasks")
+            .then(taskObject.taskRefresh());
+
+    }
 }
+
+
 
 
 export default taskObject
