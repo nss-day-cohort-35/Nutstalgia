7668b56689e645546bf82ef38f21feebe5cc0319
George Williamson <keatonwilliamson@Georges-MacBook-Pro.local>
2019-09-25 10:40:15 -0500

    add friend works and refreshed friend list

--- src/scripts/friends/eventListeners.js
+++ src/scripts/friends/eventListeners.js
@@ -1,10 +1,10 @@
 import API from "./../dataAPI.js";
 
 const friendsObject = {
-    // hiddenId: "",
     friendsListeners: () => {
         document.querySelector("#btnFriends").addEventListener("click", event => {
             friendsObject.refresh();
+            friendsObject.populateFriends();
         })
         // mainContainer.addEventListener("click", () => {
         //     taskObject.editOrDelete();
@@ -42,24 +42,13 @@ const friendsObject = {
                             API.deleteByID("requests", requestId)).then(response =>
                                 API.searchGetExpand("requests", "receiverId", sessionStorage.activeUser, "user"))
                         .then(response => friendsObject.requestsLoop(response))
+                        .then(response => friendsObject.refresh())
+                        .then(response => friendsObject.populateFriends())
                 }
-                // taskObject.displayModal();
-                // API.getByID("tasks", event.target.id.split("-")[1])
-                //     .then(response => {
-                //         taskObject.hiddenId = `${response.id}`
-                //         document.querySelector("#btnTasksSave").innerHTML = "SAVE TASK";
-                //         document.querySelector("#taskTitle").value = response.taskName
-                //         document.querySelector("#taskDetails").value = response.taskDescription
-                //         document.querySelector("#taskDate").value = response.dueDate
-                //     })
-
                 event.stopPropagation()
             }
         })
     },
-    // addFriendship: (id) => {
-    //     API.saveAnything(friendsObject.jsonObject(id), "friendships")
-    // },
     jsonObject: (id) => {
         let compoundId = `${sessionStorage.activeUser}-${id}`
         return {
@@ -85,33 +74,14 @@ const friendsObject = {
             friendsObject.displayModal();
             document.querySelector("#requestsContainer").innerHTML = ""
             API.searchGetExpand("requests", "receiverId", sessionStorage.activeUser, "user")
-                .then(response => friendsObject.requestsLoop(response));
-            // taskObject.hiddenId = ""
+                .then(response => friendsObject.requestsLoop(response))
         })
-        // API.searchGet("tasks", "userId", sessionStorage.activeUser)
-        //     .then(response => taskObject.taskLoop(response));
+
     },
-    // editOrDelete: () => {
-    //     if (event.target.id.startsWith("taskDelete")) {
-    //         const targetID = event.target.id.split("-")[1]
-    //         const confirmDelete = confirm("Do you want to delete this task?")
-    //         if (confirmDelete) {
-    //             API.deleteByID("tasks", targetID)
-    //                 .then(response => taskObject.taskRefresh());
-    //         }
-    //     } else if (event.target.id.startsWith("taskEdit")) {
-    //         taskObject.displayModal();
-    //         API.getByID("tasks", event.target.id.split("-")[1])
-    //             .then(response => {
-    //                 taskObject.hiddenId = `${response.id}`
-    //                 document.querySelector("#btnTasksSave").innerHTML = "SAVE TASK";
-    //                 document.querySelector("#taskTitle").value = response.taskName
-    //                 document.querySelector("#taskDetails").value = response.taskDescription
-    //                 document.querySelector("#taskDate").value = response.dueDate
-    //             })
-    //     }
-    //     event.stopPropagation()
-    // },
+    populateFriends: () => {
+        API.searchGetExpand("friendships", "initiatorId", sessionStorage.activeUser, "user")
+            .then(response => friendsObject.friendsLoop(response))
+    },
     requestsModalComponent: () => {
         return `
             <div id="requestsModal" class="modal">
@@ -129,15 +99,6 @@ const friendsObject = {
             </div> 
             `
     },
-    // taskComponent: (task) => {
-    //     return `<div>
-    //         <h2>title:  ${task.taskName}</h2>
-    //         <h3>description: ${task.taskDescription}</h3>
-    //         <h3>date: ${task.dueDate}</h3>
-    //         <button id="taskEdit-${task.id}">Edit</button>
-    //         <button id="taskDelete-${task.id}">Delete</button>
-    //     </div>`
-    // },
     requestComponent: (object) => {
         return `<div>
             <h3>${object.user.firstName} ${object.user.lastName} (${object.user.userName}) wants to be friends!</h3>
@@ -145,44 +106,30 @@ const friendsObject = {
             <button id="requestReject-${object.id}">Reject</button>
         </div>`
     },
+    friendComponent: (object) => {
+        return `<div>
+            <h3>${object.user.firstName} ${object.user.lastName} (${object.user.userName}) is your friend</h3>
+            <button id="removeFriend-${object.compound}">End Friendship</button>
+        </div>`
+    },
     requestsLoop: (requestsArray) => {
         document.querySelector("#requestsContainer").innerHTML = ""
         const arrayOfRequests = [...requestsArray]
         const sortedRequests = arrayOfRequests.sort((yeahhh, boiii) => boiii.id - yeahhh.id)
         sortedRequests.forEach(request => {
-            console.log(request)
+            // console.log(request)
             document.querySelector("#requestsContainer").innerHTML += friendsObject.requestComponent(request)
         });
     },
-    // taskLoop: (taskArray) => {
-    //     const arrayOfTasks = [...taskArray]
-    //     const sortedTasks = arrayOfTasks.sort((yeahhh, boiii) => boiii.id - yeahhh.id)
-    //     sortedTasks.forEach(task => {
-    //         document.querySelector("#mainContainer").innerHTML += taskObject.taskComponent(task)
-    //     });
-    // },
-    // jsonObject: (title, details, date) => {
-    //     return {
-    //         userId: sessionStorage.activeUser,
-    //         taskName: title,
-    //         taskDescription: details,
-    //         dueDate: date,
-    //         isComplete: "no"
-    //     }
-    // },
-    // postJSON: () => {
-    //     let title = document.querySelector("#taskTitle").value
-    //     let deets = document.querySelector("#taskDetails").value
-    //     let date = document.querySelector("#taskDate").value
-    //     if (taskObject.hiddenId === "") {
-    //     API.saveAnything(taskObject.jsonObject(title, deets, date), "tasks")
-    //         .then(response => taskObject.taskRefresh());
-    //     }
-    //     else {
-    //         API.putByID("tasks", taskObject.hiddenId, taskObject.jsonObject(title, deets, date))
-    //             .then(response => taskObject.taskRefresh());
-    //     }
-    // }
+    friendsLoop: (friendsArray) => {
+        friendsObject.refresh()
+        const arrayOfFriends = [...friendsArray]
+        const sortedFriends = arrayOfFriends.sort((yeahhh, boiii) => boiii.id - yeahhh.id)
+        sortedFriends.forEach(friend => {
+            // console.log(friend)
+            document.querySelector("#mainContainer").innerHTML += friendsObject.friendComponent(friend)
+        });
+    },
 }
 
 
